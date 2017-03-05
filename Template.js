7c4fe5324c9cce9634ec5c330a970d5330d71c94
Joseph Salini <josephsalini@gmail.com>
2017-03-05 01:27:32 +0100

    update template generation

--- Template.js
+++ Template.js
@@ -1,7 +1,41 @@
 
 
 class Template {
-    static getHeader(title) {
+    
+    constructor(title) {
+        this.header = document.createElement("header");
+        this.header.innerHTML = this.getHeader(title);
+        
+        this.nav = document.createElement("nav");
+        this.nav.innerHTML = this.getNavigation();
+        
+        this.bodier = document.createElement("bodier");
+
+        this.footer = document.createElement("footer");
+        this.footer.innerHTML = this.getFooter();
+        
+        this.skeleton = document.createElement("div");
+        this.skeleton.appendChild(this.header);
+        this.skeleton.appendChild(this.nav);
+        this.skeleton.appendChild(this.bodier);
+        this.skeleton.appendChild(this.footer);
+        
+        this.setContentElement(this.getBackground2());
+    }
+
+    setContentElement(contentElement) {
+        while (this.bodier.lastChild) {
+            this.bodier.removeChild(this.bodier.lastChild);
+        }
+        this.bodier.appendChild(contentElement);
+    }
+
+    apply(container) {
+        container.innerHTML = this.skeleton.innerHTML;
+    }
+
+
+    getHeader(title) {
         var str  = ''
         str += '<span id="HOME_logo"><a href="index.html"><img src="img/home.png" alt="home" height="40px"/></a></span>'
         str += '<h1>'+title+'</h1>'
@@ -14,7 +48,7 @@ class Template {
 
     }
 
-    static getNavigation() {
+    getNavigation() {
         var str  = ''
         str += '<table >'
         str += this.getNavElement('Resume.html', 'Resume');
@@ -30,11 +64,18 @@ class Template {
         return str;
     }
     
-    static getBackground() {
+    getBackground2() {
+        var bgElement = document.createElement("div");
+        bgElement.className="background_icub";
+        bgElement.innerHTML = this.getBackground();
+        return bgElement;
+    }
+    
+    getBackground() {
         return '<img  src="img/back_title_icub.png" height="100%" alt="icub background" />'
     }
     
-    static getFooter() {
+    getFooter() {
         var str  = ''
         str += '<p id="footer_description">'
         str += '<span class="date">2 Mar 2017</span> <strong>Joseph Salini</strong>'
@@ -42,11 +83,11 @@ class Template {
         return str;
     }
     
-    static getNavElement(h, e) {
+    getNavElement(h, e) {
         return '<tr><td> <h2> <a href="'+h+'">'+e+'</a> </h2> </td> </tr>'
     }
     
-    static getNavNoElement() {
+    getNavNoElement() {
         return '<tr><td> <br>  </td></tr>'
     }
 
